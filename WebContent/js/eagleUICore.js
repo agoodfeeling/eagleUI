0738891346a77b0f0a5c6abd1f7c98f6c7ee0525
agoodfeeling <agoodfeeling@163.com>
2017-01-15 01:21:43 +0800

    base

---
+++ WebContent/js/eagleUICore.js
@@ -0,0 +1,16 @@
+/**
+ * @author 英魁文
+ * @version 0.1
+ * @date 2016-11-30
+ */
+var eagleUI = {
+	startup:function() {
+		
+	}
+	/**
+	 * 
+	 */
+	,autoClosePopup:function(title,content,milisecond) {
+		
+	}
+};
